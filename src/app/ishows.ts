0d76b92cefdbfc094859bf6ffeae9757093e7351
awolln <aweln457@gmail.com>
2023-03-17 22:32:15 -0700

    searchshows

---
+++ src/app/ishows.ts
@@ -0,0 +1,8 @@
+export interface IShows {
+    movies: string
+    documentaries: string
+    news: string
+    talkshow: string
+    sport: string
+    description: string
+}
--- src/app/search-component/search-component.component.ts
+++ src/app/search-component/search-component.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { IShows } from '../ishows';
 @Component({
   selector: 'app-search-component',
   templateUrl: './search-component.component.html',
   styleUrls: ['./search-component.component.css']
 })
-export class SearchComponentComponent {
-faSearchIcon=faMagnifyingGlass;
+export class SearchComponentComponent implements OnInit   {
+
+search: IShows
+constructor() {
+  this.search = {
+    movies: 'all categories',
+    documentaries: 'hitory',
+    news: 'local and international',
+    talkshow: 'family',
+    sport: 'all categories',
+    description: 'maturity rating',
+
+
+  }
+}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
